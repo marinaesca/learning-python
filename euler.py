c619f4a0061d1b255a8e42d9acbc9c494abd9060
marinaesca <marinaescalante@utexas.edu>
2016-12-16 02:23:59 -0600

    added prompt

--- euler.py
+++ euler.py
@@ -1,3 +1,6 @@
 
 print("great googly moogly this is project euler 7")
-print("this is a change?")
+print("this is a change!")
+
+print("goal: what is the 10001st prime?")
+print("ex: 1st 6 primes: 2,3,5,7,11,13 -> 6th prime is 13")
