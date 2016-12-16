e66cbe41c7ebdddf2e1da5ae2ed88c60a6caa1ea
marinaesca <marinaescalante@utexas.edu>
2016-12-15 19:11:05 -0600

    added first python file
    Escalante file: plays with strings, printing with lists, printing for loops

---
+++ pythonStrug.py
@@ -0,0 +1,28 @@
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+print("time to learn arrays (ish)")
+
+foo = 'Marina'
+bar = 'Eskimolattes'
+baz = 'Escalator'
+dog = 'Esca-LANTES'
+cat = 'Esca'
+pig = 'Escalante'
+
+litList = [];
+litList.append(bar)
+litList.append(baz)
+litList.append(dog)
+litList.append(cat)
+litList.append(pig)
+print(litList)
+
+
+print('now it gets tricky')
+print('later learn to sub foo for marina and + in python')
+
+for last in litList:
+	print "%s M. %s" % (foo,last)
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
