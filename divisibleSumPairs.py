ffb226ac2d528f71a2de86bbe996806f81cfc093
marinaesca <marinaescalante@utexas.edu>
2016-12-20 00:30:27 -0600

    wrote program, has error taking input from file, need to learn syntax and fix, UNFINISHED

--- divisibleSumPairs.py
+++ divisibleSumPairs.py
@@ -1,2 +1,21 @@
 #marinaescalante-unfinished
 
+n = input()  # number of a nums in list
+k = input()  # number we are dividing with
+
+aList = []   # list containing the a numbers
+
+# take in aList info
+for num in range(n):
+	aList.append(input())
+
+count = 0  # how many pairs are LEGIT
+
+for i in range(n):
+	for j in range((i + 1), n):
+		if (not(i == j)) and ((i + j) % k == 0):
+			count += 1
+
+print(count)
+			
+
