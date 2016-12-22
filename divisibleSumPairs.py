27ce549b50179ad4775e2d5d00cc4dc0259a5a17
marinaesca <marinaescalante@utexas.edu>
2016-12-22 11:45:54 -0600

    error fixed, literally a parenthesis problem, finished

--- divisibleSumPairs.py
+++ divisibleSumPairs.py
@@ -1,20 +1,15 @@
-#marinaescalante-unfinished
+#marinaescalante-finished
 
-# current error: ValueError: invalid literal for int() with base 10: '<built-in'
-# ran correctly with temp data, so problem is reading in raw input
+#tempdata1 = "6 3"
+#tempdata2 = "1 3 2 6 1 2"
+#datalist1 = (tempdata1).split() # n & k
+#datalist2 = (tempdata2).split() # a list
 
-tempdata1 = "6 3"
-tempdata2 = "1 3 2 6 1 2"
-
-datalist1 = (tempdata1).split() # n & k
-datalist2 = (tempdata2).split() # a list
-
-#datalist1 = str(raw_input).split() # n & k
-#datalist2 = str(raw_input).split() # a list
+datalist1 = str(raw_input()).split() # n & k
+datalist2 = str(raw_input()).split() # a list
 
 n = int(datalist1[0])  # number of a nums in list
 k = int(datalist1[1])  # number we are dividing with
-
 aData = [int(x) for x in datalist2]
 
 aList = []   # list containing the a numbers
