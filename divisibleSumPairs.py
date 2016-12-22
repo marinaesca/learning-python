a9f806f28a6f80a888b82b4fa07d699e0c41b721
marinaesca <marinaescalante@utexas.edu>
2016-12-21 23:39:36 -0600

    divisibleSumPairs works with pre made strings in file, has errors when reading in input, unfinished

--- divisibleSumPairs.py
+++ divisibleSumPairs.py
@@ -1,13 +1,27 @@
 #marinaescalante-unfinished
 
-n = input()  # number of a nums in list
-k = input()  # number we are dividing with
+# current error: ValueError: invalid literal for int() with base 10: '<built-in'
+# ran correctly with temp data, so problem is reading in raw input
+
+tempdata1 = "6 3"
+tempdata2 = "1 3 2 6 1 2"
+
+datalist1 = (tempdata1).split() # n & k
+datalist2 = (tempdata2).split() # a list
+
+#datalist1 = str(raw_input).split() # n & k
+#datalist2 = str(raw_input).split() # a list
+
+n = int(datalist1[0])  # number of a nums in list
+k = int(datalist1[1])  # number we are dividing with
+
+aData = [int(x) for x in datalist2]
 
 aList = []   # list containing the a numbers
 
 # take in aList info
 for num in range(n):
-	aList.append(input())
+	aList.append(aData[num])
 
 count = 0  # how many pairs are LEGIT
 
