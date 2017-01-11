cc4729b68e66c8f3204e1ac268df4ed1a4b430e0
marinaesca <marinaescalante@utexas.edu>
2017-01-11 01:11:06 -0600

    learning linked list in python

---
+++ data-structures/linked_list.py
@@ -0,0 +1,73 @@
+# marinaescalante-unfinished
+
+# using bradyz template, mostly learning not my code
+
+class LinkedListNode:
+    def __init__(self, value, pointer=None):
+        self.value = value
+        self.pointer = pointer
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, value):
+        if self.head is None:
+        	self.head = LinkedListNode(value)
+        else:
+        	current = self.head
+        	while current.pointer is not None:
+        		current = current.pointer
+        	current.pointer = LinkedListNode(value)
+
+    def remove(self, value):
+        # do something to show error when trying to remove when head none?
+        if self.head.value == value:
+            current = self.head.pointer
+            self.head = current
+        else:
+            current = self.head
+            while current.pointer is not None:
+                if current.pointer.value == value:
+                    pointerNext = current.pointer.pointer
+                    current.pointer.value = None
+                    current.pointer.pointer = None
+                    current.pointer = pointerNext
+
+                current = current.pointer
+
+    def contains(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True;
+            current = current.pointer
+
+        return False
+
+    def __str__(self):
+        # No need to mess with this function -
+        # Really important to understand.
+        node_strings = list()
+
+        # Go through all the nodes and gather the string representations.
+        current = self.head
+        while current is not None:
+            node_strings.append(str(current))
+            current = current.pointer
+
+        return "->".join(node_strings)
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.head = LinkedListNode(1)
+    ll.head.pointer = LinkedListNode(2)
+    ll.head.pointer.pointer = LinkedListNode(3)
+
+    print(ll)
+
---
+++ data-structures/linked_list_tests.py
@@ -0,0 +1,103 @@
+# bradyz code, using for tests
+# adding in my own code, making test cases
+
+'''
+Implement the required functions in linked_list.py,
+then run "python linked_list_tests.py".
+'''
+
+import unittest
+from linked_list import LinkedList
+
+
+class LinkedListTest(unittest.TestCase):
+    def test_to_string(self):
+        ll = LinkedList()
+
+        self.assertEqual(str(ll), "")
+
+    def test_add_values(self):
+        ll = LinkedList()
+
+        ll.add(1)
+        ll.add(2)
+        ll.add(3)
+
+        self.assertEqual(str(ll), "1->2->3")
+
+    def test_add_values_two(self):
+        ll = LinkedList()
+
+        ll.add(3)
+        ll.add(6)
+        ll.add(1)
+        ll.add(9)
+        ll.add(9)
+        ll.add(8)
+
+        self.assertEqual(str(ll), "3->6->1->9->9->8")
+
+    def test_contains_value(self):
+        ll = LinkedList()
+
+        ll.add(1)
+        ll.add(2)
+        ll.add(3)
+
+        self.assertTrue(ll.contains(1))
+        self.assertTrue(ll.contains(2))
+        self.assertTrue(ll.contains(3))
+
+        self.assertFalse(ll.contains(10))
+
+    def test_contains_value_two(self):
+        ll = LinkedList()
+
+        ll.add(37)
+        ll.add(944)
+        ll.add(2000)
+
+        self.assertTrue(ll.contains(37))
+        self.assertTrue(ll.contains(944))
+        self.assertTrue(ll.contains(2000))
+
+        self.assertFalse(ll.contains(1))
+        self.assertFalse(ll.contains(38))
+        self.assertFalse(ll.contains(20))
+
+    def test_remove_value(self):
+        ll = LinkedList()
+
+        ll.add(1)
+        ll.add(2)
+        ll.add(3)
+
+        self.assertEqual(str(ll), "1->2->3")
+
+        ll.remove(2)
+
+        self.assertEqual(str(ll), "1->3")
+
+    def test_remove_value_two(self):
+        ll = LinkedList()
+
+        ll.add(70)
+        ll.add(80)
+        ll.add(90)
+        ll.add(900)
+        ll.add(800)
+        ll.add(700)
+
+        self.assertEqual(str(ll), "70->80->90->900->800->700")
+
+        ll.remove(90)
+
+        self.assertEqual(str(ll), "70->80->900->800->700")
+
+        ll.remove(900)
+
+        self.assertEqual(str(ll), "70->80->800->700")
+
+
+if __name__ == "__main__":
+    unittest.main()
